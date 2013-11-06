4690d127240250983ea9914a8137c05898d43e07
Clark Chung <clark.ck@gmail.com>
2013-11-06 15:53:42 +0800

    Add java solution for 101. Symmetric Tree
    101. Symmetric Tree: https://leetcode.com/problems/symmetric-tree/

---
+++ java/leetcode/symmetric_tree/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.symmetric_tree;
+
+import common.TreeNode;
+
+public class Solution {
+	public boolean isSymmetric(TreeNode root) {
+		if(root == null) return true;
+		return isSymmetric(root.left, root.right);
+	}
+	private static boolean isSymmetric(TreeNode left, TreeNode right) {
+		if(left == null && right == null) return true;
+		if(left != null && right == null) return false;
+		if(left == null && right != null) return false;
+		if(left.val != right.val) return false; 
+		return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+	}
+
+}
