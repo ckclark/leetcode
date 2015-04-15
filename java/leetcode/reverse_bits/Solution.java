e4cb54a48f96e4f97deb953b2cea1d7caedb475f
Clark Chung <clark.ck@gmail.com>
2015-04-15 10:45:49 +0800

    Add java solution for 190. Reverse Bits
    190. Reverse Bits: https://leetcode.com/problems/reverse-bits/

---
+++ java/leetcode/reverse_bits/Solution.java
@@ -0,0 +1,14 @@
+package leetcode.reverse_bits;
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int out = 0;
+        for(int i = 0; i < 32; i++){
+            out <<= 1;
+            out |= n & 1;
+            n >>= 1;
+        }
+        return out;
+    }
+}
