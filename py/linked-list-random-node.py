670df024302a30ccf8be42ec869143e1f3d4457f
Clark Chung <clark.ck@gmail.com>
2017-10-31 01:43:03 +0800

    Add py solution for 382. Linked List Random Node
    382. Linked List Random Node: https://leetcode.com/problems/linked-list-random-node/

---
+++ py/linked-list-random-node.py
@@ -0,0 +1,36 @@
+import random
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+
+    def __init__(self, head):
+        """
+        @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node.
+        :type head: ListNode
+        """
+        self.head = head
+
+    def getRandom(self):
+        """
+        Returns a random node's value.
+        :rtype: int
+        """
+        t = self.head
+        val = None
+        n = 0
+        while t is not None:
+            n += 1
+            if random.random() * n < 1:
+                val = t.val
+            t = t.next
+        return val
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
